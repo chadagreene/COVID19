e1c188aeb399d2d9e8266f0cb62b532d12308302
Chad Greene <chad@chadagreene.com>
2021-01-21 13:41:42 -0700

    one-year update
    I've made several minor adjustments to the code in the past year. Most notably, the script now defaults to plotting daily new cases rather than cumulative sums. Also, this version does a better job of removing weekly cycles, removing single-day spikes, and smoothing through time.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 
 This MATLAB script automatically downloads the latest daily statistics of known cases of COVID-19 and related deaths ([data from The New York Times](https://github.com/nytimes/covid-19-data)), then makes animations like this one: 
 
+[![Animation of covid spreading in the United States](https://img.youtube.com/vi/c3QNtyvQAho/0.jpg)](https://www.youtube.com/watch?v=c3QNtyvQAho)
+
+Alternatively, here's the output of an early version of this script:
+
 ![](coronavirus_NYT_20200401.gif)
-
-Go a little bit further and explore how the rates of doubling differ from place to place: 
-
-![](covid_case_doubling_rates.png) 
 
 ## Requirements 
 Requires the [Climate Data Toolbox for MATLAB](https://github.com/chadagreene/CDT) ([Greene et al., 2019](https://doi.org/10.1029/2019GC008392)).
--- coronavirus_map_nytimes.m
+++ coronavirus_map_nytimes.m
@@ -11,13 +11,17 @@
 % Known issues: 
 % * The NYT data lump all NYC data into one, unassociated with a fips, so 
 % it doesn't get mapped here. Kansas City also gets left out. 
+% * As of Jan 2021, this code has gotten a somewhat messy as I've made a bunch of little
+% tweaks over the course of the past year. Sorry about that. 
 % 
 %% Enter preferences:
 
 DownloadLatestData = true; 
-makegif = true; % make a gif using gif function from Climate Data Tools
+makegif = false; % make a gif using gif function from Climate Data Tools
 makemp4 = true; % makes an mp4 using videoWriter 
-
+use_export_fig = true; % export_fig makes higher resolution animations than the default getframe, but takes longer to make the animations.  
+newcases = true; % Analyze and plot "new cases" meaning the number of new cases each day rather than cumulative sums. 
+despike = true; % removes single-day spikes via a  3 day moving median after accounting for the typical weekly cycle. (This effectively throws out all the cases that get reported when a state dumps a giant load in a single day, and that usually happens when a state finds or starts counting a type of case they hadnt previously reported. Anyway, this option discards those major data dumps in favor of creating a smooth time series without distracting flashes of data in the visualization.   
 FixNYC = true;  % evenly distributes New York City values among their five counties (scaled by population). If this is false, no values will appear in the map for NYC 
 
 %% Download new data if requested: 
@@ -78,8 +82,49 @@ for k = 1:length(t)
 end
 
 % Scale by population of each county: 
-C_per10k = 10000*C./pop; % number of confirmed cases per 10,000 residents
-D_per10k = 10000*D./pop; % number of deaths per 10,000 residents
+if newcases
+   isnc = isnan(C); 
+   isnd = isnan(D); 
+   C(isnd) = 0; 
+   D(isnc) = 0; 
+   C_per10k = 10000*diff(movmean([zeros(length(x),1) C],[6 0],2),1,2)./pop; 
+   D_per10k = 10000*diff(movmean([zeros(length(x),1) D],[6 0],2),1,2)./pop;
+   %C_per10k(C_per10k==0) = NaN; 
+   %D_per10k(D_per10k==0) = NaN; 
+
+   if despike 
+      day = repmat(1:7,1,100); 
+      day = day(1:length(t)); 
+      Cd = diff([zeros(length(x),1) C],1,2);
+      Cdm = movmean(Cd,7,2); 
+
+      % Remove the weekly cycle by assuming that for each county, each day of week has a level of reporting that is a scalar multiple of that week's average. For example, Sundays might typically have half the cases of the overall week average, but Monday might have double: 
+      Cd2 = NaN(size(C));
+      for cnty = 1:size(C,1)
+         for k = 1:7
+            ind = day==k; 
+            tmp = median(Cd(cnty,ind)./Cdm(cnty,ind),'omitnan'); 
+
+            Cd2(cnty,ind) = Cd(cnty,ind) - Cd(cnty,ind).*tmp; 
+         end
+      end
+      
+      % Despike: 
+      Cd2 = movmedian(Cd,3,2); 
+      
+      % Smooth ith a gaussian filter. Previously I was using a 7 day unweighted moving average, but such a boxcar window results in jittery data because the 7 day average is always sensitive to each new datapoint the boxcar encounters.  
+      Cd2f = filter(gausswin(15)/sum(gausswin(15)),1,Cd2,[],2); 
+      
+      C_per10k = 10000*Cd2f./pop; 
+      %C_per10k = 10000*movmean(Cd2,[6 0],2)./pop; 
+
+   end
+   
+else
+   % Cumulative: 
+   C_per10k = 10000*C./pop; % number of confirmed cases per 10,000 residents
+   D_per10k = 10000*D./pop; % number of deaths per 10,000 residents
+end
 
 %% Make maps: 
 
@@ -92,24 +137,44 @@ D_per10k = 10000*D./pop; % number of deaths per 10,000 residents
 % of your liking. It might take some tinker to find color axis values that 
 % show the minor spatial variations without clipping too much on either end. 
 
-caxc = log10([0.1 40]);  % case color axis limits
-caxd = log10([0.003 3]); % death color axis limits
+linear = false; 
+
+if newcases
+   if linear 
+      caxc = [0 10]; 
+   else
+      caxc = log10([3 150]/10);  % case color axis limits
+      caxd = log10([0.01 10]/10); % death color axis limits
+   end
+else
+   caxc = log10([0.1 100]);  % case color axis limits
+   caxd = log10([0.01 10]); % death color axis limits
+end
 
 % Open a figure: 
-figure('color','k','pos',[50 50 1600 495])
+sc = 1; % size scaling factor. bigger number makes a bigger figure
+figure('color','k','pos',[50 50 800*sc 495*sc])
 
 % Start with the most recent data: 
 k=size(C_per10k,2);
 
-% PLOT THE CONFIRMED CASE DATA: 
-subplot(1,2,1) 
+cmap = cmocean('thermal'); 
 
-% Indices of finite data: 
+% % The patchsc function is in Climate Data Toolbox for Matlab (Greene et al, 2019):
+% hc = patchsc(x(ind),y(ind),log10(C_per10k(ind,k)),...
+%    'edgecolor','none','colormap',cmocean('thermal'),'caxis',caxc);
+
+% Update case data: 
 ind = C_per10k(:,k)>0; 
 
 % The patchsc function is in Climate Data Toolbox for Matlab (Greene et al, 2019):
-hc = patchsc(x(ind),y(ind),log10(C_per10k(ind,k)),...
+if linear 
+   hc = patchsc(x(ind),y(ind),(C_per10k(ind,k)),...
+      'edgecolor','none','colormap',cmocean('thermal'),'caxis',caxc);
+else 
+   hc = patchsc(x(ind),y(ind),log10(C_per10k(ind,k)),...
    'edgecolor','none','colormap',cmocean('thermal'),'caxis',caxc);
+end
 
 % Plot state boundaries for context: 
 plot(statex,statey,'color',0.65*[1 1 1])
@@ -118,102 +183,75 @@ plot(statex,statey,'color',0.65*[1 1 1])
 axis([ -2294168    2415097    2820366    5725879])
 daspect([1 1 1]) 
 axis off 
-set(gca,'position',[0 0 0.5 1])
+set(gca,'position',[0 0 1 1])
 
 % Set tick marks for the colorbar. (Here I'm setting a wider range than necessary, which should hopefully cover everything ever needed) 
 caxticks = [0.001 0.003 0.01 0.03 0.1 0.3 1 3 10 30 100 300 1000 3000 10000 30000]; 
 
 cbc = colorbar('south'); 
-cbc.Position = [0.03 0.05 0.15 0.017];
+cbc.Position = [0.05 0.05 0.3 0.017];
 cbc.Color = 0.65*[1 1 1]; 
-set(cbc,'fontsize',14,'xtick',log10(caxticks),'xticklabel',num2str(caxticks'),'xaxislocation','top')
-xlabel(cbc,'known COVID-19 cases per 10k residents') 
+set(cbc,'fontsize',14*sc,'xtick',log10(caxticks),'xticklabel',num2str(caxticks'),'xaxislocation','top')
+xlabel(cbc,'daily COVID-19 cases per 10k residents') 
 
 ax(1) = gca; % This will come in handy later. 
 
-% PLOT THE DEATH DATA: 
-subplot(1,2,2) 
-
-ind = D_per10k(:,k)>0; 
-
-% The patchsc function is in Climate Data Toolbox for Matlab (Greene et al, 2019):
-hd = patchsc(x(ind),y(ind),log10(D_per10k(ind,k)),...
-   'edgecolor','none','colormap',cmocean('thermal'),'caxis',caxd);
-
-% Plot state boundaries for context: 
-plot(statex,statey,'color',0.65*[1 1 1])
-
-% Set axis limits: (sorry Alaska and other colonies) 
-axis([ -2294168    2415097    2820366    5725879])
-daspect([1 1 1]) 
-axis off 
-set(gca,'position',[0.5 0 0.5 1])
-
-cbd = colorbar('south'); 
-cbd.Position = [0.53 0.05 0.15 0.017];
-cbd.Color = 0.65*[1 1 1]; 
-set(cbd,'fontsize',14,'xtick',log10(caxticks/100),'xticklabel',num2str(caxticks'/100),'xaxislocation','top')
-xlabel(cbd,'COVID-19 deaths per 10k residents') 
-
-ax(2) = gca; % This will come in handy later. 
-
 % The sgtitle function was introduced in 2018b. Replace with "text" if you need to: 
-txt = sgtitle(datestr(t(k),'yyyy-mmm-dd'),...
-   'fontsize',18,'color',0.65*[1 1 1],'fontweight','bold');
+txt = ntitle(datestr(t(k),'mmm dd'),...
+   'fontsize',20*sc,'color',0.65*[1 1 1],'fontweight','bold');
 
 ntitle({'animation by Chad A. Greene';'data from The New York Times'},...
-   'location','se','fontsize',10,'color',0.65*[1 1 1])
-
-linkaxes(ax,'xy') % lets you zoom in on one map and automatically adjust the extents of the other. 
+   'location','se','fontsize',10*sc,'color',0.65*[1 1 1])
 
 %% Animate: 
 
 if makemp4
    % Initialize an mp4 video: 
-   v = VideoWriter(['coronavirus_NYT_',datestr(t(end),'yyyymmdd'),'.mp4'],'MPEG-4'); 
-   v.FrameRate=8; % 8 frames per second 
+   v = VideoWriter(['coronavirus_NYT_daily_',datestr(t(end),'yyyymmdd'),'despike.mp4'],'MPEG-4'); 
+
+   v.FrameRate=16; %
    v.Quality=100; 
    open(v)
-   writeVideo(v,getframe(gcf)); % writes the first frame
+   if use_export_fig
+      fr = export_fig('-nocrop','-r500'); % can use getframe(gcf) instead, but export_fig offers more control over resolution, etc.  
+   else
+      fr = getframe(gcf); 
+   end
+   writeVideo(v,fr); % writes the first frame
 end
 
 if makegif
    % Initialize a gif and write the first frame (using Climate Data Tools):
-   gif(['coronavirus_NYT_',datestr(t(end),'yyyymmdd'),'.gif'],'frame',gcf,'delaytime',1/8)   
+   gif(['coronavirus_NYT_daily_',datestr(t(end),'yyyymmdd'),'despike2.gif'],'frame',gcf,'delaytime',1/16)   
 end
 
 % Loop through each day: 
 for k = 1:length(t) 
    
-   % Make the case axes current: 
-   axes(ax(1))
+   delete(hc(:))
    
    % Update case data: 
    ind = C_per10k(:,k)>0; 
-   delete(hc(:))
+
+   % The patchsc function is in Climate Data Toolbox for Matlab (Greene et al, 2019):
    hc = patchsc(x(ind),y(ind),log10(C_per10k(ind,k)),...
       'edgecolor','none','colormap',cmocean('thermal'),'caxis',caxc);
-   uistack(hc,'bottom') % places the county data under state boundaries 
-   
-   % Make the case axes current: 
-   axes(ax(2))
-   
-   % Update case data: 
-   ind = D_per10k(:,k)>0; 
-   delete(hd(:))
-   hd = patchsc(x(ind),y(ind),log10(D_per10k(ind,k)),...
-      'edgecolor','none','colormap',cmocean('thermal'),'caxis',caxd);
-   uistack(hd,'bottom') % places the county data under state boundaries 
-   
+
    % Update date: 
-   txt.String = datestr(t(k),'yyyy-mmm-dd'); 
+   txt.String = datestr(t(k),'mmm dd'); 
+   uistack(hc,'bottom')
    
    % Write the video frame: 
    if makegif 
       gif
    end
    if makemp4
-      writeVideo(v,getframe(gcf));
+      if use_export_fig
+         fr = export_fig('-nocrop','-r500'); % can use getframe(gcf) instead, but export_fig offers more control over resolution, etc.  
+      else
+         fr = getframe(gcf); 
+      end
+      writeVideo(v,fr); 
    end
 end
 
@@ -227,80 +265,4 @@ if makemp4
    close(v)
 end
 
-%% Analyze local trends 
-% 
-
-% Enter preferences for analysis: 
-dur = 10;     % length of window of investigation (days). Longer durations give more stable numbers, but provide a less current view of what's happening, and a shorter window also allows more counties to be included.  
-thresh = 20; % minimum # of cases on first day of the analysis window. The numbers tend to be less stable when starting with fewer than 10 or 20 cases.   
-
-% Convert to datenum for polynomial fit relative to days: 
-td = datenum(t); 
-
-% Get indices of the window length of interest: 
-indw = (length(td)-dur+1):length(td); 
-
-% Sum of cases in all counties (these numbers are lower than totals for all of US bc some cases haven't been assigned to counties yet.)  
-sumC = 10000*sum(C,'omitnan')/sum(pop,'omitnan'); 
-
-% Fit polynomials to log data: (Rates of doubling are 1/p(1) days)
-p_national = polyfit(td(indw),log(sumC(indw)),1)
-p_seattle = polyfit(td(indw),log(C_per10k(82,indw)),1)
-p_miami = polyfit(td(indw),log(C_per10k(2923,indw)),1)
-
-figure
-plot(t,sumC,'k','linewidth',2) 
-axis tight
-box off
-ylabel('known cases of COVID-19 per 10k residents') 
-set(gca,'yscale','log') 
-hold on
-plot(t,C_per10k(82,:),'color',[0.22 0.49 0.72]) % King County, WA (Seattle) 
-plot(t,C_per10k(2923,:),'color',[0.89 0.1 0.11]) % Miami-Dade, FL
-
-% Plot the linear fits: 
-plot(t(indw),exp(polyval(p_national,td(indw))),'k-')
-plot(t(indw),exp(polyval(p_seattle,td(indw))),'-','color',[0.22 0.49 0.72])
-plot(t(indw),exp(polyval(p_miami,td(indw))),'-','color',[0.89 0.1 0.11])
-
-legend('United States','King, WA (Seattle)','Miami-Dade FL','location','southeast')
-legend boxoff 
-
-%% Map the recent doubling rates for all counties
-
-% Preallocate an array for doubling rates: 
-r = NaN(size(pop)); 
-
-% Loop through each county: 
-for k = 1:length(x)
-
-   % Solve for rates only if day 1 of the analysis window has at least thresh # of cases.  
-   if C(k,indw(1))>=thresh 
-      pv = polyfit(td(indw),log(C(k,indw)),1); 
-      r(k) = 1/pv(1); 
-   end
-end
-
-ind = isfinite(r); 
-
-figure('color',0.5*[1 1 1],'pos',[50 50 840 495])
-hp = patchsc(x(ind),y(ind),r(ind),...
-   'edgecolor','none','colormap',cmocean('-balance'),...
-   'caxis',1/p_national(1) + 3*[-1 1]);
-
-plot(statex,statey,'color',0.65*[1 1 1])
-
-axis([-2294168    2415097    2820366    5725879])
-daspect([1 1 1]) 
-
-axis off 
-
-set(gca,'position',[0 0 1 1])
-
-cb = colorbar('south'); 
-cb.Position = [0.05 0.05 0.3 0.02];
-cb.Color = 0.15*[1 1 1]; 
-set(cb,'xdir','reverse','xaxislocation','top') 
-xlabel(cb,['doubling rate (days) of known cases, ',datestr(t(indw(1)),'ddmmm'),'-',datestr(t(indw(end)),'ddmmm')])
-
-%export_fig covid_case_doubling_rates.png -r300 
+disp done
